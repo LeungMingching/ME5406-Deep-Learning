995133a4ba41a06f4e40cd89f9092111db78992a
LeungMingching <liangmingjing849@163.com>
2020-10-04 21:24:44 +0800

    rename 'main' file to 'MC'
    create 'SARSA', output wrong q_table

--- SARSA.py
+++ SARSA.py
@@ -8,7 +8,8 @@ START = (0, 0)
 
 EXPLORATION_RATE = 0.2
 DISCOUNT_RATE = 0.9
-MAX_STEPS = 2000
+LEARNING_RATE = 0.9
+MAX_STEPS = 1000000
 
 
 class State:
@@ -66,14 +67,6 @@ class Agent:
                 for a in self.actions:
                     self.q_table[(i, j)][a] = 0
 
-        # initialize return
-        self.ret = {}
-        for i in range(BOARD_ROWS):
-            for j in range(BOARD_COLS):
-                self.ret[(i, j)] = {}
-                for a in self.actions:
-                    self.ret[(i, j)][a] = [0, 0]  # [sum of g, times of first visit]
-
     def pick_action(self, exp_rate=EXPLORATION_RATE):
         # default e-greedy policy with EXPLORATION RATE， input 0 to be greedy policy
         current_pos = self.State.state
@@ -111,7 +104,6 @@ class Agent:
         self.path = []
         self.State = State()
         self.step_no = 0
-        self.g = 0
 
     # def show_result(self):
     #
@@ -120,96 +112,93 @@ class Agent:
     #     for i in range(BOARD_ROWS):
     #         for j in range(BOARD_COLS):
     #             states.append((i, j))
-    # 
+    #
+    #     path_states = [s[0] for s in self.greedy_path]
+    #
     #     # scan the grid
     #     for s in states:
-    #         if s ==
+    #         if s in path_states:
 
-    def mc(self, rounds=10):
+    def sarsa(self, rounds=10):
         i = 0.0
 
+        # loop for episode
         while i < rounds:
 
-            # forward moving
+            # pick action with e-greedy policy
+            action = self.pick_action()
             while not self.State.isEnd and self.step_no < MAX_STEPS:
-                # pick action with e-greedy policy
-                action = self.pick_action()
-
-                # counting steps in 1 episode
+                # counting steps
                 self.step_no += 1
 
-                # record the path in 1 episode
+                # record the path
                 self.path.append([self.step_no, self.State.state, action])
 
                 # update State
                 self.State = self.take_action(action)
 
+                # read reward
+                reward = self.State.get_reward()
+
+                # read last_state & last_action
+                last_state = self.path[-1][1]
+                last_action = self.path[-1][2]
+
+                # choose action for next step
+                action = self.pick_action()
+
+                # update q_table
+                self.q_table[last_state][last_action] = self.q_table[last_state][last_action] + LEARNING_RATE \
+                                                        * (reward + DISCOUNT_RATE
+                                                           * self.q_table[self.State.state][action]
+                                                         - self.q_table[last_state][last_action])
+
                 # determine if game is end
                 self.State.fun_end()
 
-            # backward calculate for q_values
+                # show win or lose
+                if self.State.isEnd:
 
-            # if game ends, get reward
-            reward = self.State.get_reward()
-            for a in self.actions:
-                self.q_table[self.State.state][a] = reward
-            if reward > 0:
-                print('Bingo:', reward)
-            else:
-                print('Fail:', reward)
-
-            # loop for each step
-            for s in reversed(self.path):
-
-                self.g = DISCOUNT_RATE * self.g + reward
-                reward = self.State.get_reward(s[1])
-
-                # determinate if it is first-visit
-                self.deter_first_visit(s, self.path)
-
-                # update Q_table
-                if self.first_visit:
-                    # append g to g_list
-                    self.ret[s[1]][s[2]][0] += self.g
-                    self.ret[s[1]][s[2]][1] += 1
-
-                    self.q_table[s[1]][s[2]] = self.ret[s[1]][s[2]][0] / self.ret[s[1]][s[2]][1]
+                    if reward > 0:
+                        print('Win:', reward)
+                    elif reward < 0:
+                        print('Lose:', reward)
+                    else:
+                        print('End with reward = o. Error!!')
 
             self.reset()
             i += 1
 
-        #  final play with greedy policy (CAN BE IMPROVED)
-        while not self.State.isEnd:
+        # #  final play with greedy policy (CAN BE IMPROVED)
+        # while not self.State.isEnd:
+        #
+        #     # pick action with greedy policy
+        #     action = self.pick_action(0)
+        #
+        #     # counting steps in 1 episode
+        #     self.step_no += 1
+        #
+        #     # record the greedy path
+        #     self.greedy_path.append([self.State.state, action])
+        #
+        #     # update State
+        #     self.State = self.take_action(action)
+        #
+        #     # determine if game is end
+        #     self.State.fun_end()
+        #
+        #     if self.step_no > MAX_STEPS:
+        #         self.give_up = True
+        #         print('current optimal path cannot converge to an end. Please increase looping time...')
+        #         break
 
-            # pick action with greedy policy
-            action = self.pick_action(0)
 
-            # counting steps in 1 episode
-            self.step_no += 1
+ag = Agent()
+ag.sarsa(10000)
+print(ag.q_table)
+print('optimal path:', ag.greedy_path)
 
-            # record the greedy path
-            self.greedy_path.append([self.State.state, action])
-
-            # update State
-            self.State = self.take_action(action)
-
-            # determine if game is end
-            self.State.fun_end()
-
-            if self.step_no > MAX_STEPS:
-                self.give_up = True
-                print('current optimal path cannot converge to an end. Please increase looping time...')
-                break
-
-
-if __name__ == "__main__":
-    ag = Agent()
-    ag.mc(2000)
-    if not ag.give_up:
-        print(ag.q_table)
-        print('optimal path:', ag.greedy_path)
-
-    # put all the code in final_move in a function show_result.
-    # let self.give_up be the termination factor of the main while.
-    # complete show_result
-    # need a threshold value to terminate the whole process
+# put all the code in final_move in a function show_result.
+# let self.give_up be the termination factor of the main while.
+# complete show_result
+# need a threshold value to terminate the whole process
